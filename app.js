4f6a9a45d9f45d2232eaa54d1baa1f755826aa93
Pushparghya Mullick <pushmullick69@gmail.com>
2025-01-16 18:09:14 +0530

    cookie parser added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user-routes');
 const eventRoutes = require('./routes/event-routes');
 
@@ -9,7 +9,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 app.use('/api/events', eventRoutes);
